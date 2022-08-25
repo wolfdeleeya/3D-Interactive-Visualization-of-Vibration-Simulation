2dcafa75f969bc5d41f0117dd7ff6ad1d63e2a7b
exordiumdeveloper1 <vugdelijanikola@gmail.com>
2022-08-25 12:43:35 +0200

    solution renaming done

--- imgui_implementacija/headers/color_helper.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "glm/glm.hpp"
-#include "imgui.h"
-
-glm::vec3 convert_hsv_to_rgb(const glm::vec3 hsv);
-
-ImU32 imgui_convert_hsv_to_rgb(const glm::vec3 hsv);
--- imgui_implementacija/shaders/engine_cell_selection_shader.frag
+++
@@ -1,9 +0,0 @@
-#version 330 core
-
-flat in uvec4 frag_color;
-
-out vec4 color;
-
-void main() {
-	color = frag_color / 255.f;
-}
